2f983b7b63d6b04feebccc496a4743ab9a7682de
Jeremy Lainé <jeremy.laine@m4x.org>
2013-03-17 14:46:37 +0100

    rename jpickle.parse to jpickle.loads

--- README.md
+++ README.md
@@ -14,4 +14,4 @@ Usage
 Here is a basic example for parsing a pickled string:
 
     var jpickle = require('jpickle');
-    jpickle.parse('U\x0bhello worldq\x01.');
+    jpickle.loads('U\x0bhello worldq\x01.');
--- test/test.js
+++ test/test.js
@@ -4,36 +4,36 @@ var assert = require('assert')
 // pickle version 0
 
 // INT
-assert.equal(jpickle.parse('I1\n.'), 1);
-assert.equal(jpickle.parse('I12\n.'), 12);
+assert.equal(jpickle.loads('I1\n.'), 1);
+assert.equal(jpickle.loads('I12\n.'), 12);
 
 // STRING
-assert.equal(jpickle.parse("S'foo'\np0\n."), 'foo');
-assert.equal(jpickle.parse("S\"foo\"\np0\n."), 'foo');
+assert.equal(jpickle.loads("S'foo'\np0\n."), 'foo');
+assert.equal(jpickle.loads("S\"foo\"\np0\n."), 'foo');
 
 // DICT
-assert.deepEqual(jpickle.parse('(dp0\n.'), {});
-assert.deepEqual(jpickle.parse("(dp0\nS'foo'\np1\nS'bar'\np2\ns."), {foo: 'bar'});
+assert.deepEqual(jpickle.loads('(dp0\n.'), {});
+assert.deepEqual(jpickle.loads("(dp0\nS'foo'\np1\nS'bar'\np2\ns."), {foo: 'bar'});
 
 // pickle version 1
 
 // BININT1
-assert.equal(jpickle.parse('K\x01.'), 1);
+assert.equal(jpickle.loads('K\x01.'), 1);
 
 // BININT2
-assert.equal(jpickle.parse('M\x00\x01.'), 256);
+assert.equal(jpickle.loads('M\x00\x01.'), 256);
 
 // BININT4
-assert.equal(jpickle.parse('J\x00\x00\x01\x00.'), 65536);
+assert.equal(jpickle.loads('J\x00\x00\x01\x00.'), 65536);
 
 // BINSTRING
-assert.equal(jpickle.parse('T\x00\x01\x00\x00xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxq\x00.'), 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+assert.equal(jpickle.loads('T\x00\x01\x00\x00xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxq\x00.'), 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
 
 // SHORT_BINSTRING
-assert.equal(jpickle.parse('U\x03fooq\x00.'), 'foo');
+assert.equal(jpickle.loads('U\x03fooq\x00.'), 'foo');
 
 // EMPTY_DICT
-assert.deepEqual(jpickle.parse('}q\x00.'), {});
+assert.deepEqual(jpickle.loads('}q\x00.'), {});
 
 // SETITEMS
-assert.deepEqual(jpickle.parse('}q\x00U\x03fooq\x01U\x03barq\x02s.'), {foo: 'bar'});
+assert.deepEqual(jpickle.loads('}q\x00U\x03fooq\x01U\x03barq\x02s.'), {foo: 'bar'});
